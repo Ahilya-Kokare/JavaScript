31901a494c94f812b7458abf125d8f58d56c2afd
Ahilya-Kokare <151702825+Ahilya-Kokare@users.noreply.github.com>
2023-11-24 02:36:28 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let score1 = 33
+// let score2 = "33"
+// let score3 = "33abc"
+// let score4 = null
+// let score5 = undefined
+// let score6 = true
+
+
+
+// console.log(typeof score1);
+// console.log(typeof (score1));
+
+// console.log(typeof (score2));
+
+
+// let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber1 = Number(score3)
+// console.log(valueInNumber1);
+
+// let valueInNumber2 = Number(score4)
+// console.log(valueInNumber2);
+
+// console.log(typeof (score4));
+// console.log(typeof (score5));
+// console.log(typeof (score6));
+
+
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1 ; false =>0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn1 = ""
+// let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+// console.log(booleanIsLoggedIn1)
+
+// let isLoggedIn2 = "Ahilya"
+// let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn2)
+
+// 1=> true; 0=> false
+// "" => false
+// "Ahilya" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
