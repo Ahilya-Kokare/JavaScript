96d4b20e4072ffeb980112968b8afeed2a692f84
Ahilya-Kokare <151702825+Ahilya-Kokare@users.noreply.github.com>
2023-11-24 17:30:49 +0000

    Conversion operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -52,5 +52,39 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***Operation***
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ahilya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+
+let gameCounter = 100
+++gameCounter;
+gameCounter1++;
+console.log(gameCounter);
+console.log(gameCounter1);
